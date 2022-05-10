bf6745dfad7bc1ff6578083dd818acb9a453d8bf
pierreamir123 <pierre.farag00@eng-st.cu.edu.eg>
2022-05-10 21:20:03 +0200

    migrate

--- app/config/email.config.js
+++ app/config/email.config.js
@@ -2,5 +2,5 @@
 user = "lolosoftwaretest@gmail.com"
 pass = "lolosoftwaretest2000"
 service = "gmail"
-base_url = 'http://www.twi-jay.xyz:8080'
+base_url = 'http://www.twi-jay.me:8080'
 email_secret = "ourlittleemailsecret"
