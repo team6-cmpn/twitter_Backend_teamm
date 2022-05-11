6432c3ffa493291955f564f3ecd3b39371bd27de
pierreamir123 <pierre.farag00@eng-st.cu.edu.eg>
2022-05-11 23:52:44 +0200

    fix pusher

---
+++ app/models/notification.model.js
@@ -0,0 +1,46 @@
+const { string } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const User = require("./user.model")
+const Tweet = require("./tweet.model")
+
+
+const notificationSchema = new Schema({
+
+
+  notificationType: {
+      type: String,
+      required: true
+  },
+
+notificationHeader:{
+  type: String,
+  required: true
+},
+
+notificationContent:{
+  type: Schema.Types.ObjectId,
+  ref:"Tweet"
+},
+
+user: [
+  {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+],
+
+tweet: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+},
+
+})
+
+
+notificationSchema.methods.toJSON = function() {
+  var notification = this.toObject();
+  return notification;
+};
+
+module.exports = mongoose.model("Notification", notificationSchema );
