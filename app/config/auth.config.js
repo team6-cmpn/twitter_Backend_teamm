86d06c2b89f7bf646d5e7953128443cc93e12807
pierreamir123 <pierre.farag00@eng-st.cu.edu.eg>
2022-05-12 01:01:26 +0200

    fix pusher

---
+++ app/config/auth.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    secret: "ourtwitterlittlesecretkey",
+    jwtExpiration: 1200,           // 20 min
+    jwtRefreshExpiration: 28800,   // 8 hours
+    
+    // jwtExpiration: 40,           // 40 sec
+    // jwtRefreshExpiration: 80,   // 80 sec
+};
